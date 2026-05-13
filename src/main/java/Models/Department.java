7c84f9004a2fffa23e69ca7fc484499bc95a7c2b
carey <meliksetyan.karine@mail.ru>
2026-05-13 14:54:04 +0300

    first commit

---
+++ src/main/java/Models/Department.java
@@ -0,0 +1,17 @@
+package Models;
+
+public class Department {
+
+    private long id;
+    private String name;
+
+    public Department(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/main/java/Models/Person.java
@@ -0,0 +1,40 @@
+package Models;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private long id;
+    private String name;
+    private String gender;
+    private Department department;
+    private double salary;
+    private LocalDate birthDate;
+
+    public Person(long id,
+                  String name,
+                  String gender,
+                  Department department,
+                  double salary,
+                  LocalDate birthDate) {
+
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.department = department;
+        this.salary = salary;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
